04683199fba03cb007846f5e480b4c7a54664296
Eldar Jahijagic <ejahijagic@unifonic.com>
2022-08-07 09:12:17 +0400

    Initialize service

---
+++ src/test/java/com/ejahijagic/staffmanagementservice/StaffManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejahijagic.staffmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagementServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
